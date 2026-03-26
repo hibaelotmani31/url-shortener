e7e22d93cf0d33b4c8dceba0740a9348a1830851
Hiba El otmani <elotmani@et.esiea.fr>
2026-03-26 17:32:58 +0100

    bug q3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const app = express();
 
 const port = process.env.PORT || 3000;
-const DB_FILE = '/tmp/urls.json';
+const DB_FILE = './urls.json';
 
 app.use(cors());
 app.use(express.json());
